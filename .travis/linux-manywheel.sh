a1e6d7d96ca49ce5f71b79d95218a70a364eeda5
Mark Guzman <segfault@hasno.info>
2019-02-20 12:31:31 -0500

    bumping the minimum version. no more 2.7

--- .travis/linux-manywheel.sh
+++ .travis/linux-manywheel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eux
 
+MIN_VER=34
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 27 ]; then
+    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge $MIN_VER ]; then
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
     fi
 done
@@ -14,14 +16,14 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 27 ]; then
+    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge $MIN_VER ]; then
         "${PYBIN}/pip" install ujson --no-index -f /io/wheelhouse
     fi
 done
 
 # run tests
 for PYBIN in /opt/python/*/bin; do
-    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 27 ]; then
+    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge $MIN_VER ]; then
         "${PYBIN}/pip" install six blist unittest2 pytz
         "${PYBIN}/python" /io/tests/tests.py
     fi
